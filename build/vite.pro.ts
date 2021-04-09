7601e3d0bc7e7ec73ad08c89ed26b6e5842be11a
yanzhangshuai <yanzhangshuai@126.com>
2021-04-09 12:50:32 +0800

    feat:加入rollup-plugin-visualizer 性能检测工具

--- build/vite.pro.ts
+++ build/vite.pro.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite';
+import { UserConfigExport } from 'vite';
 import { merge } from 'webpack-merge';
+import visualizer from 'rollup-plugin-visualizer';
 import baseConfig from './vite.base';
 
-export default merge(baseConfig, defineConfig({
-    mode: 'production'
-}));
+let viteConfig = merge<UserConfigExport>(baseConfig, {
+  mode: 'production',
+});
+
+if (process.env.npm_config_report) {
+  viteConfig = merge(viteConfig, {
+    build: {
+      rollupOptions: {
+        plugins: [visualizer({
+          open: true,
+          gzipSize: true,
+          brotliSize: true,
+          template:"treemap" //| "treemap" | "network"
+        })]
+      }
+    }
+  });
+}
+
+export default viteConfig;
